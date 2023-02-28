e9fd73982f7abb8186ad6cac493505afd4306ea5
ZLW07 <205430509@qq.com>
2023-02-28 14:52:12 +0800

    Trajectory visualization

--- TestRos/src/robot_planning/src/test_random.cpp
+++ TestRos/src/robot_planning/src/test_random.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit_visual_tools/moveit_visual_tools.h>
 
 int main(int argc, char **argv)
 {
@@ -12,16 +13,14 @@ int main(int argc, char **argv)
   spinner.start();
   // 连接move_group节点中的机械臂实例组，这里的组名arm是我们之前在setup assistant中设置的
   moveit::planning_interface::MoveGroupInterface group("arm");
+  const robot_state::JointModelGroup* joint_model_group =
+      group.getCurrentState()->getJointModelGroup("arm");
 
-  ROS_INFO("================================eeeeee===========================");
-/*
+  const moveit::core::LinkModel* joint_link =
+      group.getCurrentState()->getLinkModel("link_6");
 
   geometry_msgs::Pose target_pose1;
   target_pose1.orientation.w =  0.726282;
-  target_pose1.orientation.x = 4.04423e-07;
-  target_pose1.orientation.y = -0.687396;
-  target_pose1.orientation.z = 4.81813e-07;
-
   target_pose1.position.x = 0.03;
   target_pose1.position.y = 0.2;
   target_pose1.position.z = 0.5;
@@ -33,19 +32,30 @@ int main(int argc, char **argv)
 //用布尔型变量标记运动规划是否成功
   bool success = (group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
 
-//打印结果
-  ROS_INFO("Visualizing plan 1 (pose goal) %s",success?"":"FAILED");
+  ROS_INFO_NAMED("tutorial", "Visualizing plan 1 as trajectory line");
+
+  namespace rvt = rviz_visual_tools;
+  moveit_visual_tools::MoveItVisualTools visual_tools("base_link","/rviz_visual_markers");
+
+  Eigen::Isometry3d text_pose = Eigen::Isometry3d::Identity();
+  text_pose.translation().z() = 1.15;
+
+//  visual_tools.publishAxisLabeled(target_pose1, "pose1");
+////markers创建
+  visual_tools.publishText(text_pose, "Pose Goal", rvt::WHITE, rvt::XLARGE);
+//画线
+  visual_tools.publishTrajectoryLine(my_plan.trajectory_ , joint_link,joint_model_group,rvt::WHITE);
+  visual_tools.trigger();
+//完成一次规划，与RViz 界面进行交互，使其点击 next 再继续执行。
+  visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
   if (success)
   {
     group.execute(my_plan);
   }
-*/
-
-  // 随机产生一个目标位置
-  group.setRandomTarget();
-  // 开始运动规划，并且让机械臂移动到目标位置
-  group.move();
-
+  auto target = group.getCurrentPose();
+  ROS_WARN("================%f",target.pose.position.x);
+  ROS_WARN("================%f",target.pose.position.y);
+  ROS_WARN("================%f",target.pose.position.z);
   ros::waitForShutdown();
 }
 
